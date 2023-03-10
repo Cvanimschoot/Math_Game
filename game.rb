8161647d84b70dd69d97fb767a05bc455b2adca7
Cvanimschoot <clayton.vanimschoot@gmail.com>
2023-03-09 18:29:00 -0700

    Add game class and setup file. Add player and question class.

---
+++ game.rb
@@ -0,0 +1,17 @@
+class Game
+
+  # attr_accessor :player_one, :player_two
+
+ def initialize
+  @player_one = Player.new
+  @player_two = Player.new
+  @turn = 0
+ end
+
+ def ask_question
+
+ end
+
+ 
+
+end
